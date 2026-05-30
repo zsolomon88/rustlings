23f57fc4aec4a4fa2a2ba4bb14a390bf5681ff52
Zev Solomon <zev2888@gmail.com>
2026-05-30 15:04:57 +0900

    Thru quiz 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -11,6 +11,13 @@
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(num_apples: i32) -> i32 {
+    if num_apples > 40 {
+        num_apples
+    } else {
+        num_apples * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
